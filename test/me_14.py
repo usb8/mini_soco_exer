40af7d1babe1b5f6b567f294daf21367379f2010
Harry Nguyen <hieu.fs8@gmail.com>
2025-10-14 13:31:58 +0300

    Understanding/Clean code

--- test/me_14.py
+++ test/me_14.py
@@ -13,77 +13,79 @@ def main():
     nltk.download('stopwords')
     nltk.download('wordnet')
 
-    # Load data
-    data = pd.read_csv("posts.csv")
+    # Load dataset
+    data = pd.read_csv("./test/posts.csv")
 
-    # Get a basic stopword list
+    # 1.a. Stopwords
     stop_words = stopwords.words('english')
 
-    # Add extra words to make our analysis even better
+    # 1.b. Add extra Stopwords (impact LDA).
     stop_words.extend(['would', 'best', 'always', 'amazing', 'bought', 'quick' 'people', 'new', 'fun', 'think', 'know', 'believe', 'many', 'thing', 'need', 'small', 'even', 'make', 'love', 'mean', 'fact', 'question', 'time', 'reason', 'also', 'could', 'true', 'well',  'life', 'said', 'year', 'going', 'good', 'really', 'much', 'want', 'back', 'look', 'article', 'host', 'university', 'reply', 'thanks', 'mail', 'post', 'please'])
 
-    # this object will help us lemmatise words (i.e. get the word stem)
+    # 1.c. Tokenisation, lemmatisation and filtering pipeline
     lemmatizer = WordNetLemmatizer()
-
-    # after the below for loop, we will transform each post into "bags of words" where each BOW is a set of words from one post 
-    bow_list = []
-
+    bow_list = []  # bag-of-words list  # Type: [['token1_post1', 'token2_post1'], ['token3_post2', 'token4_post2']]
     for _, row in data.iterrows():
         text = row['content']
-        tokens = word_tokenize(text.lower()) # tokenise (i.e. get the words from the post)
-        tokens = [lemmatizer.lemmatize(t) for t in tokens] # lemmatise
-        tokens = [t for t in tokens if len(t) > 2]  # filter out words with less than 3 letter s
-        tokens = [t for t in tokens if t.isalpha() and t not in stop_words] # filter out stopwords
-        # if there's at least 1 word left for this post, append to list
+        tokens = word_tokenize(text.lower())  # tokenise each post
+        tokens = [lemmatizer.lemmatize(t) for t in tokens]  # lemmatise (impact LDA)
+        tokens = [t for t in tokens if len(t) > 2]  # drop tokens shorter than 3 chars (impact LDA)
+        tokens = [t for t in tokens if t.isalpha() and t not in stop_words]  # remove non-alphabetic tokens and stopwords (impact LDA)
         if len(tokens) > 0:
             bow_list.append(tokens)
 
-    # Create dictionary and corpus
-    dictionary = Dictionary(bow_list)
-    # Filter words that appear less than 2 times or in more than 30% of posts
-    dictionary.filter_extremes(no_below=2, no_above=0.3)
-    corpus = [dictionary.doc2bow(tokens) for tokens in bow_list]
+    # 2. Create the gensim dictionary and corpus
+    dictionary = Dictionary(bow_list)  # map between words and their integer ids
+    # print(dictionary)
+    # print(dictionary.token2id)
+    # print(dictionary.token2id)
+    # print(dictionary[0])
+    dictionary.filter_extremes(no_below=2, no_above=0.3)  # remove very rare words (no_below) and very common (no_above) (impact LDA)
+    corpus = [dictionary.doc2bow(tokens) for tokens in bow_list]  # Type: BOW type ->  [[(id, count), ...], ...]
+    # print(corpus)
 
-    # We don't know at this point how many topics there are. Therefore, we try training the LDA algorithm at different topic counts, and picking the one that results in the best coherence. 
-    optimal_coherence = -100
-    optimal_lda = None
-    optimal_k = 0
+    # 3. Model selection: We don't know how many topics are there/best
+    #    -> Train LDA algorithm for a range of topic counts and pick the model with best coherence score
+    #    coherence using c_v is a common automatic metric for topic quality.
+    best_coherence_score = -100
+    best_lda_model = None
+    best_num_topics = 0
     for K in range(2, 10):
-        
-        # Train LDA model. We want to determine how we can best split the data into 4 topics
-        lda = LdaModel(corpus, num_topics=K, id2word=dictionary, passes=10, random_state=2)
+        # Train an LDA model (impact LDA) with:
+        #   K topics
+        #   the algorithm will iterate over the corpus 10 times (larger number -> slower but can help convergence).
+        #   random_state (refer ML or DataMining textbooks)
+        lda_model = LdaModel(corpus, num_topics=K, id2word=dictionary, passes=10, random_state=2)
 
-        # Now that the LDA model is done, let's see how good it is by computing its 'coherence score'
-        coherence_model = CoherenceModel(model=lda, texts=bow_list, dictionary=dictionary, coherence='c_v')
+        # Then compute the coherence score for lda_model
+        coherence_model = CoherenceModel(model=lda_model, texts=bow_list, dictionary=dictionary, coherence='c_v')
         coherence_score = coherence_model.get_coherence()
-        
-        
-        if(coherence_score > optimal_coherence):
+
+        if coherence_score > best_coherence_score:
             print(f'Trained LDA with {K} topics. Average topic coherence (higher is better): {coherence_score} which is the best so far!')
-            optimal_coherence = coherence_score
-            optimal_lda = lda
-            optimal_k = K
+            best_coherence_score = coherence_score
+            best_lda_model = lda_model
+            best_num_topics = K
         else: 
             print(f'Trained LDA with {K} topics. Average topic coherence (higher is better): {coherence_score} which is not very good.')
-            
-    # Okay, we tried many topic numbers and selected the best one. Let's see how our trained LDA model for the optimal number of topics performed.
 
-    # First, to see the topics, print top 5 most representative words per topic
-    print(f'These are the words most representative of each of the {optimal_k} topics:')
-    for i, topic in optimal_lda.print_topics(num_words=5):
+    # 4. Show topics of the chosen model
+    # Print top 5 most representative words per topic
+    print(f'These are the words most representative of each of the {best_num_topics} topics:')
+    for i, topic in best_lda_model.print_topics(num_words=5):
         print(f"Topic {i}: {topic}")
 
-    # Then, let's determine how many posts we have for each topic
+    # 5. Count dominant topic per document
     # Count the dominant topic for each document
-    topic_counts = [0] * optimal_k  # one counter per topic
+    topic_counts = [0] * best_num_topics  # one counter per topic
     for bow in corpus:
-        topic_dist = optimal_lda.get_document_topics(bow)  # list of (topic_id, probability)
-        dominant_topic = max(topic_dist, key=lambda x: x[1])[0] # find the top probability
-        topic_counts[dominant_topic] += 1 # add 1 to the most probable topic's counter
-
+        topic_dist = best_lda_model.get_document_topics(bow)  # list of (topic_id, probability)
+        dominant_topic = max(topic_dist, key=lambda x: x[1])[0]  # find the top probability
+        topic_counts[dominant_topic] += 1  # add 1 to the most probable topic's counter
     # Display the topic counts
     for i, count in enumerate(topic_counts):
         print(f"Topic {i}: {count} posts")
+
 
 if __name__ == '__main__':
     main()
