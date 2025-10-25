4bf8c3b55cca35266fed7fca55f090ad89c1d8ea
Harry Nguyen <hieu.fs8@gmail.com>
2025-10-25 12:29:28 +0300

    4.1 refactoring

--- ex4.1.py
+++ ex4.1.py
@@ -10,6 +10,10 @@ from collections import Counter
 import sqlite3
 from nltk.downloader import Downloader
 import contractions
+from cryptography.fernet import Fernet
+import json
+import re
+
 
 def main():
     # Download necessary NLTK data, without these the below functions wouldn't work
@@ -31,6 +35,79 @@ def main():
     else:
         print("All required NLTK resources are already installed.")
 
+    ENCRYPTED_FILE_PATH = 'censorship.dat'
+    fernet = Fernet('xpplx11wZUibz0E8tV8Z9mf-wwggzSrc21uQ17Qq2gg=')
+    with open(ENCRYPTED_FILE_PATH, 'rb') as encrypted_file:
+        encrypted_data = encrypted_file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    MODERATION_CONFIG = json.loads(decrypted_data)
+    TIER1_WORDS = MODERATION_CONFIG['categories']['tier1_severe_violations']['words']
+    TIER2_PHRASES = MODERATION_CONFIG['categories']['tier2_spam_scams']['phrases']
+    TIER3_WORDS = MODERATION_CONFIG['categories']['tier3_mild_profanity']['words']
+
+    # -----------------------------------------------
+    # 1. Data Preprocessing
+    # -----------------------------------------------
+    # 1.1. Load dataset
+    def load_db():
+        DB_FILE = "database.sqlite"
+        try:
+            conn = sqlite3.connect(DB_FILE)
+            print("SQLite Database connection successful")
+        except Exception as e:
+            print(f"Error: '{e}'")
+        posts_query = "SELECT id, content FROM Posts"
+        data = pd.read_sql_query(posts_query, conn)
+        # print(data.head())
+        print("- Number of posts:", len(data))
+        conn.close()
+        return data
+    data = load_db()
+
+    # 1.2. Cleaning data
+    # 1.2.1. Cleaning data 1 (Remove duplicate posts from spammers)
+    data = data.drop_duplicates(subset=["content"])
+    print("- Number of posts after removing duplicates:", len(data))
+
+    # 1.2.2. Clean data 2 for each post
+    # def clean_text(text):
+    #     # Remove Tier 1 violations (whole text)
+    #     TIER1_PATTERN = r'\b(' + '|'.join(TIER1_WORDS) + r')\b'
+    #     if re.search(TIER1_PATTERN, text, flags=re.IGNORECASE):
+    #         return None
+
+    #     # Remove Tier 2 violations (whole text)
+    #     for phrase in TIER2_PHRASES:
+    #         if re.search(re.escape(phrase), text, flags=re.IGNORECASE):
+    #             return None
+
+    #     # Remove Tier 3 words from text
+    #     TIER3_PATTERN = r'\b(' + '|'.join(TIER3_WORDS) + r')\b'
+    #     # tier3_matches = re.findall(TIER3_PATTERN, text, flags=re.IGNORECASE)
+    #     # if tier3_matches:
+    #     #     print("Original text:", text)
+    #     #     print("---Found Tier 3 words to remove:", tier3_matches)
+    #     #     text = re.sub(TIER3_PATTERN, '', text, flags=re.IGNORECASE)
+    #     #     print("Cleaned text:", text)
+    #     text = re.sub(TIER3_PATTERN, '', text, flags=re.IGNORECASE)
+
+    #     # Remove URLs
+    #     URL_PATTERN = r'\b(https?://\S+|www\.\S+)|[a-z0-9-]+(\.|\[\.\])[a-z]{2,4}/?[a-z0-9-]*\b'
+    #     text = re.sub(URL_PATTERN, '', text, flags=re.IGNORECASE)
+
+    #     return text
+
+    # 1.3. Tokenisation, POS tagging & lemmatisation, stopword removal
+    # 1.3.1. Prepare stopwords list
+    stop_words = stopwords.words('english')
+    # Add extra Stopwords from Exercise 14 (impact LDA).
+    stop_words.extend(['would', 'good', 'always', 'amazing', 'buy', 'quick', 'people', 'new', 'fun', 'think', 'know', 'believe', 'many', 'thing', 'need', 'small', 'even', 'make', 'love', 'mean', 'fact', 'question', 'time', 'reason', 'also', 'could', 'true', 'well',  'life', 'said', 'year', 'going', 'good', 'really', 'much', 'want', 'back', 'look', 'article', 'host', 'university', 'reply', 'thanks', 'mail', 'post', 'please'])
+    # Add extra Stopwords from Zipf's law in 1.3.3
+    stop_words.extend(['attend', 'watch', 'spend', 'listen' , 'discover', 'night', 'weekend','like', 'feel', 'get', 'try', 'finish', 'see', 'wait', 'change', 'find', 'take', 'read', 'let', 'never', 'keep', 'come', 'learn', 'give', 'run', 'bring', 'enter', 'hit', 'talk', 'say', 'turn', 'push', 'open', 'lose', 'visit', 'miss', 'finally', 'else', 'still', 'truly', 'nothing', 'next', 'actually', 'damn', 'shit', 'fuck', 'seriously', 'tonight', 'bit', 'everything', 'another', 'something', 'home', 'day', 'morning', 'hour', 'first', 'last', 'next', 'today', 'everyone', 'tip', 'way', 'little', 'hard', 'real', 'perfect', 'great', 'worth'])
+    # Remove duplicates
+    stop_words = set(stop_words)
+    
+    # 1.3.2. Tokenisation, POS tagging & lemmatisation, stopword removal
     def get_wordnet_pos(tag):
         if tag.startswith('N'):
             return wordnet.NOUN
@@ -41,42 +118,16 @@ def main():
         if tag.startswith('R'):
             return wordnet.ADV
         return wordnet.NOUN  # or None
-
-    # Load dataset
-    # data = pd.read_csv("./test/posts.csv")
-    DB_FILE = "database.sqlite"
-    try:
-        conn = sqlite3.connect(DB_FILE)
-        print("SQLite Database connection successful")
-    except Exception as e:
-        print(f"Error: '{e}'")
-    posts_query = "SELECT id, content FROM Posts"
-    data = pd.read_sql_query(posts_query, conn)
-    # print(data.head())
-    # print(len(data))
-    data = data.drop_duplicates(subset=["content"])  # Remove duplicate posts from spammers
-    # print(len(data))
-    conn.close()
-
-    # 1.a. Stopwords
-    stop_words = stopwords.words('english')
-
-    # 1.b. Add extra Stopwords (impact LDA).
-    stop_words.extend(['would', 'good', 'always', 'amazing', 'buy', 'quick', 'people', 'new', 'fun', 'think', 'know', 'believe', 'many', 'thing', 'need', 'small', 'even', 'make', 'love', 'mean', 'fact', 'question', 'time', 'reason', 'also', 'could', 'true', 'well',  'life', 'said', 'year', 'going', 'good', 'really', 'much', 'want', 'back', 'look', 'article', 'host', 'university', 'reply', 'thanks', 'mail', 'post', 'please'])
     
-    # stop_words.extend(['anyone', 'someone', 'every', 'one', 'one', 'get', 'use', 'try', 'way', 'today', 'sure', 'sometimes', 'together', 'right'])  # From Zipf's law 1d and the results of LDA's topics
-    # Consider: 'first', 'highly', 'made', 'went', 'saw', 'stay', 'help', 'meet', 'learned', 'learns', 'tried', 'trying', 'considering', 'buying' -> 'learned', 'learns', 'tried', 'trying': something wrong with lemmatisation!!! -> Let fix it
-
-    # From Zipf's law 1d and the results of LDA's topics, after fix lemmatisation
-    stop_words.extend(['attend', 'watch', 'spend', 'listen' , 'discover', 'night', 'weekend','like', 'feel', 'get', 'try', 'finish', 'see', 'wait', 'change', 'find', 'take', 'read', 'let', 'never', 'keep', 'come', 'learn', 'give', 'run', 'bring', 'enter', 'hit', 'talk', 'say', 'turn', 'push', 'open', 'lose', 'visit', 'miss', 'finally', 'else', 'still', 'truly', 'nothing', 'next', 'actually', 'damn', 'shit', 'fuck', 'seriously', 'tonight', 'bit', 'everything', 'another', 'something', 'home', 'day', 'morning', 'hour', 'first', 'last', 'next', 'today', 'everyone', 'tip', 'way', 'little', 'hard', 'real', 'perfect', 'great', 'worth'])
-    stop_words = set(stop_words)
-
-    # 1.c. Tokenisation, lemmatisation and filtering pipeline``
     lemmatizer = WordNetLemmatizer()
     bow_list = []  # bag-of-words list  # Type: [['token1_post1', 'token2_post1'], ['token3_post2', 'token4_post2']]
     for _, row in data.iterrows():
-        # text = row['content']
-        text = contractions.fix(row['content'])
+        text = row['content']
+        # text = contractions.fix(row['content'])
+
+        # text = clean_text(text)
+        # if text is None:
+        #     continue
         tokens = word_tokenize(text.lower())  # tokenise each post
        
         # Problem without POS: WordNetLemmatizer treats words as nouns by default. Many verbs and adjectives will not be transformed to their base form without knowing the word POS.
@@ -89,12 +140,14 @@ def main():
         if len(tokens) > 0:
             bow_list.append(tokens)
 
-    # # 1.d. Zipf's Law: Most frequent words are stopwords (e.g., 'the', 'is', 'in', 'and', etc.)
+    # # 1.3.3. Zipf's Law: Most frequent words are stopwords (e.g., 'the', 'is', 'in', 'and', etc.)
     # _bow_list = [t for b in bow_list for t in b]
     # word_freq = Counter(_bow_list)
     # print(word_freq.most_common(200))  # print the 200 most common words
 
+    # -----------------------------------------------
     # 2. Create the gensim dictionary and corpus
+    # -----------------------------------------------
     dictionary = Dictionary(bow_list)  # map between words and their integer ids
     # print(dictionary)
     # print(dictionary.token2id)
@@ -104,9 +157,11 @@ def main():
     corpus = [dictionary.doc2bow(tokens) for tokens in bow_list]  # Type: BOW type ->  [[(id, count), ...], ...]
     # print(corpus)
 
+    # -----------------------------------------------
     # 3. Model selection: We don't know how many topics are there/best
     #    -> Train LDA algorithm for a range of topic counts and pick the model with best coherence score
     #    coherence using c_v is a common automatic metric for topic quality.
+    # -----------------------------------------------
     best_coherence_score = -100
     best_lda_model = None
     lda_model_k_10 = None
@@ -133,7 +188,9 @@ def main():
         if K == 10:
             lda_model_k_10 = lda_model
 
+    # -----------------------------------------------
     # 4. Show topics of the chosen model
+    # -----------------------------------------------
     # Print top 5 most representative words per topic
     print(f'These are the words most representative of each of the {best_num_topics} topics:')
     for i, topic in best_lda_model.print_topics(num_words=5):
@@ -145,7 +202,9 @@ def main():
         print(f"Topic {i}: {topic}")
     print('================================================')
 
+    # -----------------------------------------------
     # 5. Count dominant topic per document
+    # -----------------------------------------------
     # Count the dominant topic for each document
     topic_counts = [0] * best_num_topics  # one counter per topic
     for bow in corpus:
